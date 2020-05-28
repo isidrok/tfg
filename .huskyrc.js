def13da7668fc440f262e3e3d602e70326c935bc
Isidro Torregrosa <isistorre95@gmail.com>
2020-05-28 16:47:51 +0200

    chore(commits): add commitlint

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   hooks: {
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'pre-commit': 'lint-staged',
     'post-checkout': 'yarn build:changed',
     'post-merge': 'yarn build:changed',
