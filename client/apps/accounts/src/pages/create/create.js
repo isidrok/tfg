690fb58d4f49d7ec476d93bd1587a732dc71bd36
Isidro Torregrosa <isistorre95@gmail.com>
2020-05-28 17:43:36 +0200

    feat(cuentas): cambiar titulo

--- client/apps/accounts/src/pages/create/create.js
+++ client/apps/accounts/src/pages/create/create.js
@@ -3,7 +3,7 @@ import {Component} from '@tfg-core/component';
 class TFGAccountsCreate extends Component {
   render() {
     return this.html`
-      <h3>New Account</h3>
+      <h3>Create new account</h3>
     `;
   }
 }
