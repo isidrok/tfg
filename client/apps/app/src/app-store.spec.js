8acc48c952e6e6cdda877d151b8f50720abf8a4b
Isidrok <isistorre95@gmail.com>
2020-08-31 22:31:23 +0200

    test(app): correct app store test

--- client/apps/app/src/app-store.spec.js
+++ client/apps/app/src/app-store.spec.js
@@ -1,6 +1,6 @@
 import {appStore} from './app-store';
 describe('appStore', function () {
   it('should have the correct namespace', () => {
-    expect(appStore.namespace).toBe('apps');
+    expect(appStore.namespace).toBe('app');
   });
 });
