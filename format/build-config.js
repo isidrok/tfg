ddb868dff16c6d452b74260a2c526e74cdfba055
Isidrok <isistorre95@gmail.com>
2020-09-08 16:33:48 +0200

    foo

--- format/build-config.js
+++ format/build-config.js
@@ -3,6 +3,5 @@ const {rollupConfig} = require('@tfg-builder/builder');
 module.exports = rollupConfig.project({
   input: {
     '@tfg-core/format': 'index.js',
-    '@tfg-core/format/mask': 'src/mask.js',
   },
 });
